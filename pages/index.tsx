b989d6f42c4a793fe9b7fe21795103cfd560764f
Daniel Steigerwald <daniel@steigerwald.cz>
2022-10-27 00:30:37 +0200

    Ensure that my thumb reaches EvoluListItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
+import clsx from "clsx";
 import { useEvoluFirstDataAreLoaded } from "evolu";
 import { FC, ReactNode } from "react";
-import { ScrollView, View } from "../components/styled";
 import { ClientOnly } from "../components/ClientOnly";
 import { CreateEvolu } from "../components/CreateEvolu";
 import { EvoluFilter } from "../components/EvoluFilter";
 import { EvoluList } from "../components/EvoluList";
 import { PageTitle } from "../components/PageTitle";
+import { ScrollView, View } from "../components/styled";
 
-const ContentContainer: FC<{ children: ReactNode; isLoaded: boolean }> = ({
-  children,
-  isLoaded,
-}) => {
+const ContentContainer: FC<{
+  children: ReactNode;
+  isLoaded: boolean;
+  textClassName?: string;
+}> = ({ children, isLoaded, textClassName }) => {
   return (
     <View
-      className="mx-auto w-full max-w-[500px] p-4"
+      className={clsx("mx-auto w-full max-w-[500px] p-4", textClassName)}
       style={!isLoaded && { display: "none" }}
     >
       {children}
@@ -31,7 +33,11 @@ const Index = () => {
       <View className="flex-1 bg-white dark:bg-black">
         <ClientOnly>
           <ScrollView className="flex-1" centerContent>
-            <ContentContainer isLoaded={dataAreLoaded}>
+            <ContentContainer
+              isLoaded={dataAreLoaded}
+              // Ensure that my thumb reaches EvoluListItem.
+              textClassName="py-[30vh]"
+            >
               <EvoluList />
               <CreateEvolu />
             </ContentContainer>
