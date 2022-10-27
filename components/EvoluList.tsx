c4d3f0e812eebcc8adc53f1459dacb06a0842818
Daniel Steigerwald <daniel@steigerwald.cz>
2022-10-27 20:29:04 +0200

    Ensure the list is rendered at the same time as CreateEvolu setTitle("")

--- components/EvoluList.tsx
+++ components/EvoluList.tsx
@@ -1,3 +1,4 @@
+import { useDeferredValue, useMemo } from "react";
 import { useQuery } from "../lib/db";
 import { EvoluListItem } from "./EvoluListItem";
 
@@ -5,12 +6,18 @@ export const EvoluList = () => {
   const { rows } = useQuery((db) =>
     db.selectFrom("evolu").select(["id", "title"]).orderBy("createdAt")
   );
+  const deferredRows = useDeferredValue(rows);
 
-  return (
-    <>
-      {rows.map((row) => (
-        <EvoluListItem key={row.id} row={row} />
-      ))}
-    </>
+  // Ensure the list is rendered at the same time as CreateEvolu setTitle("").
+  // https://github.com/reactwg/react-18/discussions/86#discussioncomment-1345270
+  return useMemo(
+    () => (
+      <>
+        {deferredRows.map((row) => (
+          <EvoluListItem key={row.id} row={row} />
+        ))}
+      </>
+    ),
+    [deferredRows]
   );
 };
