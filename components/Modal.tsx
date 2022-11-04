c43be544a36915afbe13407cf4597224a490e242
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-04 16:09:06 +0100

    Create Modal.tsx

---
+++ components/Modal.tsx
@@ -0,0 +1,34 @@
+import { IO } from "fp-ts/IO";
+import { FC, ReactNode } from "react";
+import { useIntl } from "react-intl";
+import { Modal as RnModal } from "react-native";
+import { Pressable } from "./styled";
+
+export type ModalProps = {
+  children: ReactNode;
+  visible: boolean;
+  onRequestClose: IO<void>;
+};
+
+export const Modal: FC<ModalProps> = ({
+  visible,
+  children,
+  onRequestClose,
+}) => {
+  const intl = useIntl();
+
+  return (
+    <RnModal transparent onRequestClose={onRequestClose} visible={visible}>
+      <Pressable
+        accessibilityRole="button"
+        accessibilityLabel={intl.formatMessage({
+          defaultMessage: "Close",
+          id: "rbrahO",
+        })}
+        className="absolute inset-0 focus:outline-none focus-visible:ring-2 focus-visible:ring-inset"
+        onPress={onRequestClose}
+      />
+      {children}
+    </RnModal>
+  );
+};
