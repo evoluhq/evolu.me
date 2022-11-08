7e968917359566f335f3bbb9988d634c463e4b81
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-08 19:33:27 +0100

    Add bundle analyzer

--- components/styled.tsx
+++ components/styled.tsx
@@ -12,3 +12,19 @@ export const ScrollView = styled(RnScrollView);
 export const Text = styled(RnText);
 export const TextInput = styled(RnTextInput);
 export const View = styled(RnView);
+
+// To test build without Nativewind, use this code:
+
+// import {
+//   Pressable as RnPressable,
+//   ScrollView as RnScrollView,
+//   Text as RnText,
+//   TextInput as RnTextInput,
+//   View as RnView,
+// } from "react-native";
+
+// export const Pressable = RnPressable;
+// export const ScrollView = RnScrollView;
+// export const Text = RnText;
+// export const TextInput = RnTextInput;
+// export const View = RnView;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Waiting for RNfW 0.19
@@ -30,4 +34,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
