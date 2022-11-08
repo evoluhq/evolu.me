09c1f2441ca155c6c069288296ce11257a61bb4f
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-09 00:58:49 +0100

    Add variants

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -2,18 +2,24 @@ import clsx from "clsx";
 import { FC, ReactNode } from "react";
 import { Text } from "./styled";
 
-export const Heading: FC<{ children: ReactNode; level?: 1 | 2 | 3 }> = ({
-  children,
-  level = 1,
-}) => {
+const variantStyles = {
+  1: "text-3xl",
+  2: "text-2xl",
+  3: "text-xl",
+};
+
+export const Heading: FC<{
+  children: ReactNode;
+  level?: keyof typeof variantStyles;
+}> = ({ children, level = 1 }) => {
   return (
     <Text
       // @ts-expect-error RNfW
       accessibilityRole="paragraph"
       accessibilityLevel={level}
       className={clsx(
-        "text-gray-900 dark:text-gray-200 dark:antialiased",
-        level === 3 ? "text-lg" : level === 2 ? "text-xl" : "text-3xl"
+        "mb-4 mt-2 text-gray-900 dark:text-gray-200 dark:antialiased",
+        variantStyles[level]
       )}
     >
       {children}
--- components/TextButton.tsx
+++ components/TextButton.tsx
@@ -1,23 +1,52 @@
+import clsx from "clsx";
 import { forwardRef } from "react";
 import { PressableProps, View as RnView } from "react-native";
-import { Pressable, Text } from "./styled";
+import { Paragraph } from "./Paragraph";
+import { Pressable, Text, View } from "./styled";
+
+const variantStyles = {
+  primary: "dark:bg-gray-800 bg-gray-200",
+  text: "",
+};
 
 export type TextButtonProps = Omit<PressableProps, "children"> & {
   title: string;
+  variant?: keyof typeof variantStyles;
+  description?: string;
 };
 
 export const TextButton = forwardRef<RnView, TextButtonProps>(
-  function TextButton({ title, ...props }, ref) {
-    return (
+  function TextButton(
+    { title, variant = "primary", description, ...props },
+    ref
+  ) {
+    const pressable = (
       <Pressable
         {...props}
         ref={ref}
-        className="rounded ring-inset focus:outline-none focus-visible:ring-2"
+        className={clsx(
+          "rounded ring-inset focus:outline-none focus-visible:ring-2",
+          variantStyles[variant]
+        )}
       >
-        <Text className="p-2 text-lg text-gray-900 dark:text-gray-200 dark:antialiased">
+        <Text
+          selectable={false}
+          className="p-2 text-lg text-gray-900 dark:text-gray-200 dark:antialiased"
+        >
           {title}
         </Text>
       </Pressable>
+    );
+
+    if (!description) return pressable;
+
+    return (
+      <View className="mb-3">
+        <View className="mb-1 flex-row">{pressable}</View>
+        <View className="px-2">
+          <Paragraph>{description}</Paragraph>
+        </View>
+      </View>
     );
   }
 );
