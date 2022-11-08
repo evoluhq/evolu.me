484d415a94d0ec0b4b5e006512383e506955dcc8
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-09 00:58:53 +0100

    Create Ring.tsx

---
+++ components/Ring.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactNode } from "react";
+import { View } from "./styled";
+
+export const Ring: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <View className="rounded bg-white px-2 ring-1 ring-gray-300 dark:bg-black dark:ring-gray-800">
+      {children}
+    </View>
+  );
+};
