50551f7cd88a089301e5691621fd661454c42e80
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-13 22:47:27 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,9 +5,17 @@ import { createHooks, model, String1000 } from "evolu";
 export const EvoluId = model.id<"evolu">();
 export type EvoluId = model.infer<typeof EvoluId>;
 
+export const EvoluRelationId = model.id<"evoluRelation">();
+export type EvoluRelationId = model.infer<typeof EvoluRelationId>;
+
 export const { useQuery, useMutation } = createHooks({
   evolu: {
     id: EvoluId,
     title: String1000,
+  },
+  evoluRelation: {
+    id: EvoluRelationId,
+    a: EvoluId,
+    b: EvoluId,
   },
 });
