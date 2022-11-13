ea4f80209bf9b9da6a950256763503e30055c933
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-14 00:10:49 +0100

    Use styleVariant

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,16 +1,11 @@
 import clsx from "clsx";
 import { FC, ReactNode } from "react";
+import { styleVariant } from "../lib/styleVariant";
 import { Text } from "./styled";
-
-const variantStyles = {
-  1: "text-3xl",
-  2: "text-2xl",
-  3: "text-xl",
-};
 
 export const Heading: FC<{
   children: ReactNode;
-  level?: keyof typeof variantStyles;
+  level?: 1 | 2 | 3;
 }> = ({ children, level = 1 }) => {
   return (
     <Text
@@ -19,7 +14,11 @@ export const Heading: FC<{
       accessibilityLevel={level}
       className={clsx(
         "mb-4 mt-2 text-gray-900 dark:text-gray-200 dark:antialiased",
-        variantStyles[level]
+        styleVariant(level, {
+          1: "text-3xl",
+          2: "text-2xl",
+          3: "text-xl",
+        })
       )}
     >
       {children}
