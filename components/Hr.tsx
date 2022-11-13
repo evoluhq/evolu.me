781bfd294d61c7eacfa824bf6562d1284a5dae49
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-13 22:47:01 +0100

    Create Hr.tsx

---
+++ components/Hr.tsx
@@ -0,0 +1,7 @@
+import { constTrue } from "fp-ts/function";
+import { memo } from "react";
+import { View } from "./styled";
+
+export const Hr = memo(function Hr() {
+  return <View className="my-4 h-[1px] w-full bg-gray-300 dark:bg-gray-800" />;
+}, constTrue);
