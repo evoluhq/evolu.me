bc9e0fb23d30c9593deff00bdb8bb40f7325bb0c
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-21 10:15:59 +0100

    Workaround for Safari bug

--- components/Popover.tsx
+++ components/Popover.tsx
@@ -32,6 +32,7 @@ export const Popover: FC<PopoverProps> = ({
   const viewRef = useRef<View>(null);
   const [xy, setXY] = useState<XY | null>(null);
 
+  // This is web only for now.
   useLayoutEffect(() => {
     if (
       ownerRef == null ||
@@ -41,7 +42,8 @@ export const Popover: FC<PopoverProps> = ({
     )
       return;
 
-    // I was lazy to refactor callbacks to Task
+    // Refactor measureInWindow callbacks to Task would be better,
+    // but this is good enough for now.
     const ownerEl = ownerRef.current as unknown as HTMLElement;
     const viewEl = viewRef.current as unknown as HTMLElement;
 
@@ -57,13 +59,20 @@ export const Popover: FC<PopoverProps> = ({
               x: ownerRect.left + ownerRect.width,
               y: ownerRect.top - viewEl.offsetHeight + ownerRect.height,
             };
-      setXY(xy);
+      setXY((previous) => {
+        if (previous && xy.x === previous.x && xy.y === previous.y)
+          return previous;
+        return xy;
+      });
     };
 
     handleChange();
 
+    // https://github.com/necolas/react-native-web/issues/2430
+    window.visualViewport?.addEventListener("resize", handleChange);
     const subscription = Dimensions.addEventListener("change", handleChange);
     return () => {
+      window.visualViewport?.removeEventListener("resize", handleChange);
       subscription.remove();
     };
   }, [ownerRef, position]);
