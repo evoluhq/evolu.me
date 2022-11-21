c5932c6eb69987724f9104614c5dce82a7e12933
Daniel Steigerwald <daniel@steigerwald.cz>
2022-11-21 10:24:26 +0100

    Safari again

--- components/EvoluButton.tsx
+++ components/EvoluButton.tsx
@@ -151,7 +151,7 @@ export const EvoluButton = forwardRef<RnView, EvoluButton>(function EvoluButton(
         <View
           className={clsx(
             "h-3 w-3 rounded-sm bg-gray-200 transition-transform duration-100 group-focus-visible:ring-2 dark:bg-gray-800",
-            popoverIsVisible && "rotate-45"
+            popoverIsVisible && "rotate-45 transform-gpu"
           )}
         />
       </Pressable>
