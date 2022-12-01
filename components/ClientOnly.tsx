036690dc5ff8b28d186c866fff5e165aec7afbd5
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-01 21:57:47 +0100

    Update dev comment

--- components/ClientOnly.tsx
+++ components/ClientOnly.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useEffect, useState } from "react";
 
-// Because of atomWithStorage, it can't be rendered on the server.
-// It will be replaced with Next.js 13 "use client".
+// atomWithStorage can't be rendered on the server.
+// TODO: Use Next.js 13 "use client" when ready for production.
 export const ClientOnly = ({ children }: { children: ReactNode }) => {
   const [hasMounted, setHasMounted] = useState(false);
 
