282c25286acd376d8eb47d42cca75a044efea5b9
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-04 22:04:08 +0100

    Update NodeListItem.tsx

--- components/NodeListItem.tsx
+++ components/NodeListItem.tsx
@@ -1,7 +1,9 @@
 import { NonEmptyString1000 } from "evolu";
+import { constVoid } from "fp-ts/lib/function";
 import { memo } from "react";
 import { NodeId } from "../lib/db";
 import { Link } from "./Link";
+import { NodeListItemButton } from "./NodeListItemButton";
 import { View } from "./styled";
 import { Text } from "./Text";
 
@@ -20,8 +22,16 @@ export const NodeListItem = memo<NodeListItemProps>(function NodeListItem({
 }) {
   return (
     <View className="flex-row" accessibilityRole={"listitem" as "list"}>
+      <NodeListItemButton
+        focusable={false}
+        onFocus={constVoid}
+        id={id}
+        title={title}
+      />
       <Link href={`/#${id}`}>
-        <Text as="a">{title}</Text>
+        <Text as="a" className="pl-0">
+          {title}
+        </Text>
       </Link>
     </View>
   );
