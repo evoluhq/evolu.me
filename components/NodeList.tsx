9c61e1904e161b962044d37945d86700fec0db18
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-04 21:45:06 +0100

    Update NodeList.tsx

--- components/NodeList.tsx
+++ components/NodeList.tsx
@@ -1,190 +1,65 @@
 /* eslint-disable formatjs/no-literal-string-in-jsx */
+import { has, model } from "evolu";
+import { useMemo } from "react";
 import { useIntl } from "react-intl";
+import { useQuery } from "../lib/db";
+import { useLocationHashNodeIds } from "../lib/hooks/useLocationHashNodeIds";
+import { NodeListItem } from "./NodeListItem";
 import { View } from "./styled";
 import { Text } from "./Text";
 
 export const NodeList = () => {
   const intl = useIntl();
+  const ids = useLocationHashNodeIds();
 
-  // TODO: A space for scrolling.
-  //  className="py-[132px]" // 3x44
+  const { rows } = useQuery((db) => {
+    let q = db
+      .selectFrom("node")
+      .select(["id", "title"])
+      .orderBy("createdAt")
+      .where("isDeleted", "is not", model.cast(true));
+
+    ids.forEach((relatedId) => {
+      q = q.where("id", "in", (qb) =>
+        qb
+          .selectFrom("edge")
+          .where("b", "=", relatedId)
+          .select("a as id")
+          .union(
+            qb.selectFrom("edge").where("a", "=", relatedId).select("b as id")
+          )
+      );
+    });
+
+    return q;
+  });
+
+  const loadedRows = useMemo(() => rows.filter(has(["title"])), [rows]);
+
+  if (loadedRows.length === 0)
+    return ids.length === 0 ? (
+      <Text className="text-center">
+        {intl.formatMessage({
+          defaultMessage: "Here will be your thoughts, organized.",
+          id: "9Udtvr",
+        })}
+      </Text>
+    ) : null;
 
   return (
-    <View>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      {/* <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text>
-      <Text className="text-center">
-        {intl.formatMessage({
-          defaultMessage: "Here will be your thoughts, organized.",
-          id: "9Udtvr",
-        })}
-      </Text> */}
+    <View
+      accessibilityRole="list"
+      className="py-[132px]" // A space for scrolling, 3x44
+    >
+      {loadedRows.map((row) => (
+        <NodeListItem
+          key={row.id}
+          row={row}
+          // x={i}
+          // focusable={i === x}
+          // isLast={i === loadedRows.length - 1}
+        />
+      ))}
     </View>
   );
 };
