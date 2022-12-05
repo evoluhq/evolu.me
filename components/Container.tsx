f2b1a31d90a465a26949654efcb7e777f6dcbaec
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-05 09:22:32 +0100

    Remove unnecessary prop

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,7 +5,6 @@ export const Container = styled(View, "mx-auto w-full max-w-[500px] p-3", {
   variants: {
     backdrop: {
       true: "bg-white/[.85] backdrop-blur-md dark:bg-black/[.65] dark:backdrop-blur-md",
-      false: "",
     },
   },
 });
