429f3593125fbd36924b6dfff7908319f8100787
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-07 22:28:25 +0100

    Move Hr to old code

--- old-code/Hr.tsx
+++ old-code/Hr.tsx
@@ -1,7 +1,8 @@
 import { constTrue } from "fp-ts/function";
 import { memo } from "react";
-import { View } from "./styled";
+import { View } from "../components/styled";
 
+// TODO: Styled.
 export const Hr = memo(function Hr() {
   return <View className="my-4 h-[1px] w-full bg-gray-300 dark:bg-gray-800" />;
 }, constTrue);
