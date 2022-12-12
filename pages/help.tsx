b7e97f323ce2c0b233293de872bd7f7882a39206
Daniel Steigerwald <daniel@steigerwald.cz>
2022-12-12 18:12:47 +0100

    Update help.tsx

--- pages/help.tsx
+++ pages/help.tsx
@@ -3,6 +3,17 @@ import { Layout } from "../components/Layout";
 import { Text } from "../components/Text";
 
 // TODO: Explain graph, filter, key navigation, etc.
+// A Graph is a collection of nodes and connections.
+// Any node in the graph can be connected to any other node in the graph.
+// Your notes are messy, unstructured and completely unconnected?
+// On a knowledge graph, you create nodes instead of notes,
+// these are dynamic building blocks that you can connect to other pieces of
+// information. Beautiful visualisations of your knowledge...
+
+// Evolu Me is a tool for organizing thoughts.
+// Write thoughts your don't want to remember but also you don't want
+// to forget. Connect anything with anything. Here are some examples:
+
 const Help = () => {
   const intl = useIntl();
 
