c685ae28c6daca51e1ee9e32a395413212e63aec
Daniel Steigerwald <daniel@steigerwald.cz>
2023-01-02 11:41:51 +0100

    Replace favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,11 +30,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="color-scheme" content="dark light" />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -46,7 +41,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <JotaiProvider>
         <IntlProvider locale="en">
