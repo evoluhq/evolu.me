af7d879c0cc3e64c52926a975c92fc82e75d38d0
Daniel Steigerwald <daniel@steigerwald.cz>
2023-01-06 01:22:54 +0100

    Improve clearAllLocalStorageKeys

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,11 +1,26 @@
-export const localStorageKeys = {
-  newNodeAtom: "evolume:newNodeAtom",
-  editNodeAtom: "evolume:editNodeAtom",
-};
+import { array, option, string } from "fp-ts";
+import { flow, identity, pipe } from "fp-ts/function";
+import { IO } from "fp-ts/IO";
+
+const localStorageKeyPrefix = "evolume:";
+
+export const createLocalStorageKey = (s: string) =>
+  `${localStorageKeyPrefix}${s}`;
 
 // Must be called before Evolu restoreOwner and resetOwner.
-export const clearAllLocalStorageKeys = () => {
-  Object.values(localStorageKeys).forEach((value) => {
-    localStorage.removeItem(value);
+export const clearAllLocalStorageEvoluMeKeys: IO<void> = () => {
+  pipe(
+    array.makeBy(localStorage.length, identity),
+    array.filterMap(
+      flow(
+        (i) => localStorage.key(i),
+        option.fromNullable,
+        option.chain(
+          option.fromPredicate(string.startsWith(localStorageKeyPrefix))
+        )
+      )
+    )
+  ).forEach((key) => {
+    localStorage.removeItem(key);
   });
 };
