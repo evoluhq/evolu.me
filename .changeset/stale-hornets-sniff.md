5bd45ddb0cb174bf48757015bec9f8cf64e1fc95
Daniel Steigerwald <daniel@steigerwald.cz>
2024-03-08 20:05:04 +0100

    Add links to the changelog and issues

---
+++ .changeset/stale-hornets-sniff.md
@@ -0,0 +1,5 @@
+---
+"evolu.me": minor
+---
+
+Add links to the changelog and issues
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,20 +8,15 @@ export default function About() {
       <Text tag="p">
         I want all my apps to be open-sourced and local-first, so I started with
         a calendar and notes. I often use a calendar for notes and notes as a
-        calendar, so I suppose it should be just one app.
+        calendar, so I suppose it should be just one app. Apps are just
+        artificial and arbitrary boundaries within data.
       </Text>
-      <Text tag="p">
-        Apps are just artificial and arbitrary boundaries within data.
-      </Text>
-      <Text tag="p">
-        I often switch between desktop and mobile, so my apps must support
-        multiple input modes (touch, mouse, keyboard). I don&apos;t have a
-        preferred device screen, so my apps must have a fluid UI.
-      </Text>
-      <Link href="https://github.com/evoluhq/evolu.me">
-        github.com/evoluhq/evolu.me
+      {/* <Text tag="h2">Links:</Text> */}
+      <Link href="https://github.com/evoluhq/evolu.me/blob/main/CHANGELOG.md">
+        Changelog
       </Link>
-      {/* <Text tag="p">TODO: Explain how this app is secure, etc.</Text> */}
+      <Link href="https://github.com/evoluhq/evolu.me/issues">Issues</Link>
+      <Link href="https://github.com/evoluhq/evolu.me">GitHub repository</Link>
     </PageWithTitle>
   );
 }
