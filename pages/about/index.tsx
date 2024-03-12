e9dfad1cfebca5a24f7dbb484ba914d8d383a8d9
Daniel Steigerwald <daniel@steigerwald.cz>
2024-03-12 14:27:59 +0100

    Move Evolu.me motto to paragraph

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -8,8 +8,10 @@ export default function About() {
       <Text tag="p">
         I want all my apps to be open-sourced and local-first, so I started with
         a calendar and notes. I often use a calendar for notes and notes as a
-        calendar, so I suppose it should be just one app. Apps are just
-        artificial and arbitrary boundaries within data.
+        calendar, so I suppose it should be just one app.
+      </Text>
+      <Text tag="p">
+        Apps are just artificial and arbitrary boundaries within data.
       </Text>
       {/* <Text tag="h2">Links:</Text> */}
       <Link href="https://github.com/evoluhq/evolu.me/blob/main/CHANGELOG.md">
